38b9d8b8f0eeac9f53489abd673baccad814a713
mkajman <marah@atlas.cz>
2016-02-21 21:41:39 +0100

    Created reducer and redux store on the client side. React components connected with redux.

--- client/test/components/Voting_spec.jsx
+++ client/test/components/Voting_spec.jsx
@@ -2,7 +2,7 @@ import TestUtils from 'react-addons-test-utils';
 import React from 'react';
 import {List} from 'immutable';
 import ReactDOM from 'react-dom';
-import Voting from '../../src/components/Voting';
+import {Voting} from '../../src/components/Voting';
 import {expect} from 'chai';
 
 const {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate}
